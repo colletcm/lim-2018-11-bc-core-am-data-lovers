5e801786dac553cd194210a6700df3613cd82a74
AndreaRamirezM <gracielarmatos@gmail.com>
2018-12-17 07:42:15 -0500

    JS Pruebas unitarias de filtro debilidades

--- src/data.js
+++ src/data.js
@@ -1,8 +1,36 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example; */
+
+/*let datos = LOL.data;
+
+for (let i=0; i<datos.length; i++) {
+  console.log(datos.id)
+}; */
+
+/*
+-  Almacenar data del prompt
+- Comparar valor del prompt con valor del objeto
+- Si es igual, jalar dato del objeto ingresado
+- Imprimir en consola
+*/
+
+
+/* const jugador = prompt("¿De que jugador quieres saber el key? (Azir, Aatrox, Ahri)");
+let name = Object.keys(LOL.data);
+
+//recorriendo campeones para averiguar el Key
+for (let i = 0; i < name.length; i++) {
+  if (name[i] === jugador) {
+    let jugadorAnalizado = name[i];
+    for (let y = 0; y < name.length; y++) {
+
+    }
+  }
+}
+*/
---
+++ src/main.js
@@ -0,0 +1,7 @@
+//const search = document.getElementById(cuadrobusqueda);
+const btningresar = document.getElementById('ingresar');
+
+/*btningresar.addEventListener('click', function(){
+    let string = document.getElementById('cajadetexto').innerHTML = 'string';
+  
+});*/
