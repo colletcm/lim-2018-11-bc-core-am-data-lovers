990dce6f13b6ae2a06ea26980f3c4148ac16e0fb
AndreaRamirezM <gracielarmatos@gmail.com>
2018-12-18 07:59:55 -0500

    JS Pruebas unitarias de insercion de HTML en JS y uso de FOREACH

--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
-//const search = document.getElementById(cuadrobusqueda);
-const btningresar = document.getElementById('ingresar');
+//PRUEBA 000 | PROBANDO FOREACH 
+/* const espaciorespuesta = document.getElementById('respuestaid');
+const  datos = POKEMON.pokemon;
+datos.forEach(function(entry) {
+    document.write(entry);
+}); */
 
-/*btningresar.addEventListener('click', function(){
-    let string = document.getElementById('cajadetexto').innerHTML = 'string';
-  
-});*/
+
+
+// PRUEBA 001 | PROBANDO HTML EN CADENAS DE JS
+/*const respuestaid = document.getElementById('respuestaid');
+const datos = POKEMON.pokemon[0];
+const pruebatemplate =  `
+<div class="POKEMON">
+   <h2>
+       ${datos.id}
+   </h2>
+   <p class="location">${datos.name}</p>
+   <img src=${datos.img}>
+</div>
+`;
+document.body.innerHTML = pruebatemplate;*/
+
+
+//PRUEBA 002 | PRONANDO PILDORA POTTER
+const datos = POKEMON.pokemon;
+let lista = ' ';
+
+datos.forEach((datos) => {
+    const li = `
+        <li> ${datos} </li>
+    `;
+    lista += li;
+})
+
+const ulcontainer = document.getElementById('container-list');
+console.log (lista);
+
+
+
+
