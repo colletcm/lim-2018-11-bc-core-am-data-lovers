c8952ecd65755bc46bc77bda513e7c772f8683f4
AndreaRamirezM <gracielarmatos@gmail.com>
2018-12-21 16:20:11 -0500

    Cambios de data, index, main ,css para el demo

--- src/main.js
+++ src/main.js
@@ -1,41 +1,48 @@
-//PRUEBA 000 | PROBANDO FOREACH 
-/* const espaciorespuesta = document.getElementById('respuestaid');
-const  datos = POKEMON.pokemon;
-datos.forEach(function(entry) {
-    document.write(entry);
-}); */
+const root =document.getElementById('root');
+const arrayPokemon = POKEMON.pokemon;
+
+const lista = (data) => {
+  let string = '';
+  data.forEach((arrayPokemon) => {
+    const carta = `
+      <div class = "list"></div>
+      ${arrayPokemon.name}
+      <img class= "imagen" src=${arrayPokemon.img}> 
+      `;
+    string += carta;
+  });
+  root.innerHTML = string;
+};
+
+  
+lista(arrayPokemon);// invocar a la funcion 
+
+//listaData(POKEMON.pokemon);
+
+//console.log (arrayPokemon);
+// // TABLA DE POKEMON c/u para entender la DATA console.table(POKEMON.pokemon[0]); 
 
 
+// // LLamo primero por ID, la lista de opciones que tengo que esta dentro de un select
+// const opciones = document.getElementById('opcionesTipo');
+// // Al escuchar el Click me haga algo, con el E qe es el objetivo
+// opciones.addEventListener('click',(e) => {
 
-// PRUEBA 001 | PROBANDO HTML EN CADENAS DE JS
-/*const respuestaid = document.getElementById('respuestaid');
-const datos = POKEMON.pokemon[0];
-const pruebatemplate =  `
-<div class="POKEMON">
-   <h2>
-       ${datos.id}
-   </h2>
-   <p class="location">${datos.name}</p>
-   <img src=${datos.img}>
-</div>
-`;
-document.body.innerHTML = pruebatemplate;*/
+// console.log(prueba.consultarTipo(e.target.value));
+// // Llamo al mensaje por su id, para que se inserte en el HTML
+//   document.getElementById('mensaje').innerHTML = prueba.consultarTipo(e.target.value)
+// });
 
 
-//PRUEBA 002 | PRONANDO PILDORA POTTER
-const datos = POKEMON.pokemon;
-let lista = ' ';
+// const datos = POKEMON.pokemon[0]; // este es vulvasaur
+// const pruebavalorDos =  `
+//   <div class="POKEMON">
+//    <h2 class="id">
+//      ${datos.id}
+//    </h2>
+//    <p class="location">${datos.name}</p>
+//    <img class= "imagen" src=${datos.img}>
+//   </div>`;
 
-datos.forEach((datos) => {
-    const li = `
-        <li> ${datos} </li>
-    `;
-    lista += li;
-})
-
-const ulcontainer = document.getElementById('container-list');
-console.log (lista);
-
-
-
-
+// // eslint-disable-next-line no-unused-vars
+// const pruebavalor = Object.entries(datos);
